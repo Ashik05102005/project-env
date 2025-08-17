c5a3c8e01a78314bdb838bda88fe12f4e25572fc
Ashik05102005 <lllashik2@gmail.com>
2025-08-17 23:29:24 +0530

    html basic page

--- except.py
+++ except.py
@@ -7,6 +7,6 @@ while c==1:
         print(result)
     except  ZeroDivisionError :
          print("cant divide")
-    except  ValueError :
+    except  ValueError :  
          print("check the data type")
     c=int(input("DO YOU WANT TO CONTINUE(0/1):"))
